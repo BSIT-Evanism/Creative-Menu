764e57992c501b4a93aaa7d67ddac315a235f44d
BSIT-Evanism <71175442+BSIT-Evanism@users.noreply.github.com>
2023-09-17 17:46:30 +0000

    added hover logics

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Cursor from './components/Cursor';
+import FloatingMenu from './components/FloatingMenu';
+
+
+function App() {
+  return (
+    <div className='overflow-hidden'>
+      <Cursor />
+      <FloatingMenu />
+    </div>
+  )
+}
+
+export default App;
