5df0a3519d4121e440815e1533aa9325eb9a0bc9
BSIT-Evanism <71175442+BSIT-Evanism@users.noreply.github.com>
2023-09-18 17:45:16 +0000

    fixed mask

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-
-import { useState } from 'react';
-import FloatingMenu from './components/FloatingMenu';
-import './index.css';
+import FloatingMenu from "./components/FloatingMenu";
+import "./index.css";
 
 function App() {
-  const [cursor,setCursor] = useState(true)
-
   return (
     <>
-      <FloatingMenu setCursor={setCursor} />
-    <div className='flex w-full h-full justify-center items-center'>
+    <div className="h-full">
+      {/* <div className="mask">
+        <ul>
+          <li>About</li>
+          <li>About</li>
+          <li>About</li>
+        </ul>
+      </div> */}
+      <FloatingMenu />
+      <div className="body">
+        <h1>My Portfolio</h1>
+      </div>
     </div>
     </>
-  )
+  );
 }
 
 export default App;
